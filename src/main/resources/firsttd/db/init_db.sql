51ab32241af7e38338017672442979c71b260b00
NickyHariniaina <nickyharini1na@gmail.com>
2025-12-06 12:11:18 +0300

    feat: add user and database creation scripts

---
+++ src/main/resources/firsttd/db/init_db.sql
@@ -0,0 +1,19 @@
+-- I used PL/SQL to create the database and the user to not let people create it twice...
+
+DO
+'database_creation'
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'product_management_db') THEN
+    CREATE DATABASE product_management_db;
+  END IF;
+END
+'database_creation';
+
+DO
+'user_creation'
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'product_management_db') THEN
+    CREATE USER product_management_db;
+  END IF;
+END
+'user_creation';
