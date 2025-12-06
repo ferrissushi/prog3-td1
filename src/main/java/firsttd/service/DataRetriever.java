98a82fca74adf66c0935e832be9616b79186e7e3
NickyHariniaina <nickyharini1na@gmail.com>
2025-12-06 16:52:51 +0300

    feat: add getAllProduct method...

--- src/main/java/firsttd/service/DataRetriever.java
+++ src/main/java/firsttd/service/DataRetriever.java
@@ -2,9 +2,14 @@ package firsttd.service;
 
 import firsttd.config.DBConnection;
 import firsttd.model.Category;
+import firsttd.model.Product;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,4 +37,42 @@ public class DataRetriever {
       throw new RuntimeException("Cannot retrieve Categories: " + e);
     }
   }
+
+  public List<Product> getProductList(int page, int size) {
+    int offset = size * (page - 1) + 1;
+    String sql = """
+    SELECT p.id, p.name, p.creation_datetime, c.id as category_id, c.name as category_name
+    FROM product p join product_category c
+    ON p.id = c.product_id
+    LIMIT ? OFFSET ?;
+    """;
+    List<Product> products = new ArrayList<>();
+    try(Connection conn = dbConnection.getDBConnection();
+      PreparedStatement ps = conn.prepareStatement(sql);
+    ) {
+      ps.setInt(1, size);
+      ps.setInt(2, offset);
+      ResultSet rs = ps.executeQuery();
+      while(rs.next()) {
+        int categoryId = rs.getInt("category_id");
+        String categoryName = rs.getString("category_name");
+        Category category = new Category(categoryId, categoryName);
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        Timestamp creationDatetimeTimestamp = rs.getTimestamp("creation_datetime");
+        Instant creationDatetime = creationDatetimeTimestamp.toInstant();
+        Product product = new Product(id, name, creationDatetime, category);
+        products.add(product);
+      }
+      return products;
+    } catch(Exception e) {
+      throw new RuntimeException("Cannot retrieve products: " + e);
+    }
+  }
 }
+
+
+
+
+
+
