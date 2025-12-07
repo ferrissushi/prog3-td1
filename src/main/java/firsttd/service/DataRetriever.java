292a6e3e5a14917984f9314058fe17822f5609be
NickyHariniaina <nickyharini1na@gmail.com>
2025-12-07 16:15:23 +0300

    feat: add pagination to filter

--- src/main/java/firsttd/service/DataRetriever.java
+++ src/main/java/firsttd/service/DataRetriever.java
@@ -159,4 +159,100 @@ public class DataRetriever {
       throw new RuntimeException("Cannot retrieve products: " + e);
     }
   }
+
+  public List<Product> getProductsByCriteria(
+      String productName,
+      String categoryName,
+      Instant creationMin,
+      Instant creationMax,
+      int page,
+      int size) {
+    int offset = size * (page - 1) + 1;
+    String sql =
+        """
+        SELECT p.id, p.name, p.creation_datetime, c.name as category_name, c.id as category_id
+        FROM product p JOIN product_category c
+        ON p.id = c.product_id
+        """;
+    List<Product> products = new ArrayList<>();
+    List<String> conditions = new ArrayList<>();
+    String productNameWhereStatement = null;
+    String categoryNameWhereStatement = null;
+    String creationMinWhereStatement = null;
+    String creationMaxWhereStatement = null;
+    if (productName != null && !productName.isBlank()) {
+      productNameWhereStatement =
+          """
+          p.name ILIKE ?
+          """;
+    }
+    if (categoryName != null && !categoryName.isBlank()) {
+      categoryNameWhereStatement =
+          """
+          c.name ILIKE ?
+          """;
+    }
+    if (creationMin != null) {
+      creationMinWhereStatement =
+          """
+          p.creation_datetime >= ?
+          """;
+    }
+    if (creationMax != null) {
+      creationMaxWhereStatement =
+          """
+          p.creation_datetime <= ?
+          """;
+    }
+    if (categoryNameWhereStatement != null
+        || productNameWhereStatement != null
+        || creationMinWhereStatement != null
+        || creationMaxWhereStatement != null) {
+      sql += " WHERE ";
+      conditions =
+          Stream.of(
+                  productNameWhereStatement,
+                  categoryNameWhereStatement,
+                  creationMinWhereStatement,
+                  creationMaxWhereStatement)
+              .filter(Objects::nonNull)
+              .toList();
+      String whereStatements = String.join(" AND ", conditions);
+    }
+    sql += " LIMIT ? OFFSET ?;";
+    try (Connection conn = dbConnection.getDBConnection();
+        PreparedStatement ps = conn.prepareStatement(sql); ) {
+      int i = 0;
+      for (; i < conditions.size(); i++) {
+        if (conditions.get(i).equals(productNameWhereStatement)) {
+          ps.setString(i + 1, "%" + productName + "%");
+        } else if (conditions.get(i).equals(categoryNameWhereStatement)) {
+          ps.setString(i + 1, "%" + categoryName + "%");
+        } else if (conditions.get(i).equals(creationMinWhereStatement)) {
+          ps.setTimestamp(i + 1, Timestamp.from(creationMin));
+        } else if (conditions.get(i).equals(creationMaxWhereStatement)) {
+          ps.setTimestamp(i + 1, Timestamp.from(creationMax));
+        }
+      }
+
+      ps.setInt(i, size);
+      ps.setInt(i + 1, offset);
+
+      ResultSet rs = ps.executeQuery();
+      while (rs.next()) {
+        int categoryIdinDB = rs.getInt("category_id");
+        String categoryNameinDB = rs.getString("category_name");
+        Category category = new Category(categoryIdinDB, categoryNameinDB);
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        Timestamp creationDatetimeTimestamp = rs.getTimestamp("creation_datetime");
+        Instant creationDatetime = creationDatetimeTimestamp.toInstant();
+        Product product = new Product(id, name, creationDatetime, category);
+        products.add(product);
+      }
+      return products;
+    } catch (Exception e) {
+      throw new RuntimeException("Cannot retrieve products: " + e);
+    }
+  }
 }
